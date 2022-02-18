393a8d52ab46225d31f7fb480a16c43ee04641b3
Sarah Gonzalez Blanco <segonzalezblanco1@buffs.wtamu.edu>
2022-02-17 20:49:21 -0600

    adding assignment 2 files to github

---
+++ tests/data.sql
@@ -0,0 +1,8 @@
+/* INSERT INTO user (username, password)
+VALUES
+  ('test', ''),
+  ('other', '');
+
+INSERT INTO post (title, body, author_id, created)
+VALUES
+  ('test title', 'test' || x'0a' || 'body', 1, '2018-01-01 00:00:00'); */
